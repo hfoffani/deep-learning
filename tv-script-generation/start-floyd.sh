cbefc2873e6290b1e08a32ac58caa6f107c8ea2e
hfoffani <hfoffani@gmail.com>
2017-04-25 19:57:53 +0200

    Fix script to start and stop floyd
    Floyd CLI outputs to standard output and standard error.

--- tv-script-generation/start-floyd.sh
+++ tv-script-generation/start-floyd.sh
@@ -1,4 +1,4 @@
 # floyd init tv-script-generation
-floyd run --gpu --env tensorflow-1.0 --mode jupyter | \
+floyd run --gpu --env tensorflow-1.0 --mode jupyter 2>&1 | \
 tee /dev/tty | \
 grep 'floyd logs' | sed 's/logs/stop/' > stop-floyd.sh
